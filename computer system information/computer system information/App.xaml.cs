9645c73d3c4e03298f123938848694fff6b19f81
Onchari Wycliffe <onchariwycliffe@gmail.com>
2017-09-05 20:29:19 +0300

    #0001 the  first commit - start

---
+++ computer system information/computer system information/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace computer_system_information
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
